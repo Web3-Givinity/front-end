a18cda86524661907e89616dc28c69be3d49a2f7
atoenggDev <kromosom06@gmail.com>
2025-01-20 13:05:53 +0700

    update: ui path active in navbar

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,7 +1,9 @@
+import { Navbar } from "@/components/fragments/Navbar"
 
 const categories = () => {
   return (
     <div>
+      <Navbar />
       <h1>Categories</h1>
     </div>
   )
--- src/components/fragments/Navbar.tsx
+++ src/components/fragments/Navbar.tsx
@@ -1,9 +1,15 @@
+"use client"
+
 import { navMenu } from "@/constants/navMenu"
 import Button from "../elements/buttons/Button"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 
 export const Navbar = () => {
+
+    const pathActive = usePathname()
+
     return (
         <>
             <nav className="container max-w-[1220px] m-4 top-0 left-0 fixed bg-white rounded-[30px] z-10">
@@ -13,7 +19,7 @@ export const Navbar = () => {
                         <ul className="flex items-center gap-12">
                             {
                                 navMenu.map((item, index) => (
-                                    <li key={index} className="font-medium">
+                                    <li key={index} className={`font-medium px-4 py-1 rounded-full transition duration-200 ease-in-out hover:text-primary-400 hover:bg-[#DEF2FA] ${pathActive === item.path ? "text-primary-400 bg-[#DEF2FA] " : "text-black"}`}>
                                         <Link href={item.path}>{item.name}</Link>
                                     </li>
                                 ))
