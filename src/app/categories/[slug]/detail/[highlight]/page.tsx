65ddc3e6b1bba9b88a1fd7f4e05c36d38e8b16c6
atoenggDev <kromosom06@gmail.com>
2025-01-31 12:44:13 +0700

    added: table in detail categories page

--- src/app/categories/[slug]/detail/[highlight]/page.tsx
+++ src/app/categories/[slug]/detail/[highlight]/page.tsx
@@ -9,6 +9,7 @@ import { ProgresDetailCatSection } from '@/app/categories/components/section/Pro
 import { RelatedProjectSection } from '@/app/categories/components/section/RelatedProjectSection';
 import { ModalDonate } from '@/app/categories/components/fragments/ModalDonate';
 import { useState } from 'react';
+import { dataDonors } from '@/constants/dataDonors';
 
 
 export default function DetailHighlight({ params }: { params: { slug: string; highlight: string } }) {
@@ -36,9 +37,53 @@ export default function DetailHighlight({ params }: { params: { slug: string; hi
                 {/* progres detail section*/}
                 <ProgresDetailCatSection />
 
+
+                <section className='px-14 mt-24'>
+                    <h1 className='text-[45px] font-semibold mb-4'>Donors</h1>
+                    <table className="w-full border-separate border-spacing-y-4">
+                        <thead className={`bg-primary-400 border-gray-200 border-b-2 text-white`}>
+                            <tr className='rounded-lg'>
+                                <th className="w-12 p-3 text-sm font-semibold tracking-wide text-left">No</th>
+                                <th className="p-3 text-sm font-semibold tracking-wide text-left">Nama Lengkap</th>
+                                <th className="p-3 text-sm font-semibold tracking-wide text-left">Jenis Kelamin</th>
+                                <th className="p-3 text-sm font-semibold tracking-wide text-left">Alamat</th>
+                                <th className="p-3 text-sm font-semibold tracking-wide text-left">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody className={`divide-y divide-gray-100`}>
+
+                            {
+                                dataDonors.map((item, index) => (
+
+                                    <tr className={`bg-white shadow-lg `} key={index}>
+                                        <td className={`p-3 text-sm}  whitespace-nowrap`}>
+                                            {index + 1}
+                                        </td>
+                                        <td className={`p-3 text-sm whitespace-nowrap`}>
+                                            {item.name}
+                                        </td>
+                                        <td className={`p-3 text-sm whitespace-nowrap`}>
+                                            {item.jenisKelamin}
+                                        </td>
+                                        <td className={`p-3 text-sm  whitespace-nowrap`}>
+                                            {item.alamat}
+                                        </td>
+                                        <td className={`p-3 text-sm  whitespace-nowrap`}>
+                                            {item.status}
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+
+                        </tbody>
+                    </table>
+                </section>
+
                 {/* project relate section */}
                 <RelatedProjectSection />
             </main>
+
+            {/* modal donate */}
             <ModalDonate isVisible={showModal} onClose={() => setShowModal(false)} />
             <Footer />
         </>
---
+++ src/constants/dataDonors.ts
@@ -0,0 +1,37 @@
+export const dataDonors = [
+    {
+        id: 1,
+        name: 'Raditya Dika',
+        jenisKelamin: 'laki-laki',
+        alamat: 'sukoharjo',
+        status: 'donate'
+    },
+    {
+        id: 2,
+        name: 'Leni',
+        jenisKelamin: 'Perempuan',
+        alamat: 'Surabaya',
+        status: 'donate'
+    },
+    {
+        id: 3,
+        name: 'Rangga',
+        jenisKelamin: 'laki-laki',
+        alamat: 'Pasuruan',
+        status: 'donate'
+    },
+    {
+        id: 4,
+        name: 'Sumiarti',
+        jenisKelamin: 'Perempuan',
+        alamat: 'Ponorogo',
+        status: 'donate'
+    },
+    {
+        id: 5,
+        name: 'Yeni indriyani',
+        jenisKelamin: 'Perempuan',
+        alamat: 'Mojokerto',
+        status: 'donate'
+    },
+]
